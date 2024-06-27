3baf9f649debf253e92c16006a474a37b8244bfe
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-06-26 19:13:38 -0700

    Added a POST route for submitting notes

--- routes/notes.js
+++ routes/notes.js
@@ -6,7 +6,32 @@ const {
   } = require('../helpers/fsUtils');
 
 const uuid = require('../helpers/uuid');
-//Get routes for retrieving all the notes
+
+//GET route for retrieving all the notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+// POST Route for submitting notes
+notes.post("/", (req, res) => {
+	const { title, text } = req.body;
+
+	if (title && text) {
+		const newNotes = {
+			title,
+			text,
+			note_id: uuid(),
+		};
+
+		readAndAppend(newNotes, "./db/db.json");
+
+		const response = {
+			status: "success",
+			body: newNotes,
+		};
+
+		res.json(response);
+	} else {
+		res.json("Error in posting notes");
+	}
+});
