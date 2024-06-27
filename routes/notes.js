2b8a4bba8e83b8deb20e3ac728bca09a96a1284c
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-06-26 20:01:35 -0700

    Added a DELETE route for deleting the notes

--- routes/notes.js
+++ routes/notes.js
@@ -3,35 +3,48 @@ const {
     readFromFile,
     readAndAppend,
     writeToFile,
-  } = require('../helpers/fsUtils');
+} = require('../helpers/fsUtils');
 
 const uuid = require('../helpers/uuid');
 
 //GET route for retrieving all the notes
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
-
+});
 // POST Route for submitting notes
 notes.post("/", (req, res) => {
-	const { title, text } = req.body;
+    const { title, text } = req.body;
 
-	if (title && text) {
-		const newNotes = {
-			title,
-			text,
-			note_id: uuid(),
-		};
+    if (title && text) {
+        const newNotes = {
+            title,
+            text,
+            note_id: uuid(),
+        };
 
-		readAndAppend(newNotes, "./db/db.json");
+        readAndAppend(newNotes, "./db/db.json");
 
-		const response = {
-			status: "success",
-			body: newNotes,
-		};
+        const response = {
+            status: "success",
+            body: newNotes,
+        };
 
-		res.json(response);
-	} else {
-		res.json("Error in posting notes");
-	}
+        res.json(response);
+    } else {
+        res.json("Error in posting notes");
+    }
 });
+// DELETE Route for deleting notes
+notes.delete("/:note_id", (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile("./db/db.json")
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            console.log(json);
+            const result = json.filter((note) => note.note_id !== noteId);
+            writeToFile("./db/db.json", result);
+            res.json(`Item ${noteId} has been deleted :wastebasket:`);
+        });
+});
+
+module.exports = notes;
