3529971f620637da0d92b977a67a6c3238872189
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-06-26 19:09:15 -0700

    Added a GET route to retrieve all the notes in db.json file

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,12 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
 const uuid = require('../helpers/uuid');
+//Get routes for retrieving all the notes
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
